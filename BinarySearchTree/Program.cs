13ad966531bb6cd51932a20029b54a90478cf2e0
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-12-28 17:32:33 +0700

    membuat method preorder

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -83,6 +83,21 @@ namespace BinarySearchTree
                 inorder(ptr.rightchild);
             }
         }
+
+        public void preorder(Node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                Console.WriteLine(ptr.info + "");
+                preorder(ptr.leftchild);
+                preorder(ptr.rightchild);
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
