6851efabbb05ec2820377a54c629060ae37ead79
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-12-28 17:11:58 +0700

    membuat method public nnode

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -5,9 +5,19 @@ namespace BinarySearchTree
     class Node
     {
         public string info;
-        public string leftchild;
-        public string rightchild;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the Node Class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
     }
+
+    //A node class consists of three things, the information, references to the right child, and references to the left child
     class Program
     {
         static void Main(string[] args)
