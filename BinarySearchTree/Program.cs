699cf0574cc0c6ed443493e8092d07687a0be99a
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-12-28 17:41:28 +0700

    membuat menu pilihnan

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -117,7 +117,19 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Program x = new Program();
-            
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implementasi insert option");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("\nEnter your choice (1-5): ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+
+            }
         }
     }
 }
