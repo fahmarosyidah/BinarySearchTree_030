e7193bb5ce3b9577ad30904a600bef03291f6158
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-12-28 17:07:44 +0700

    membuat class Node dan deklarasi variabel

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -2,6 +2,12 @@
 
 namespace BinarySearchTree
 {
+    class Node
+    {
+        public string info;
+        public string leftchild;
+        public string rightchild;
+    }
     class Program
     {
         static void Main(string[] args)
