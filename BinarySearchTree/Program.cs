3d2acf18589181a0fb9fe601f58a860e312545ba
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-12-28 17:25:39 +0700

    membuat method untuk memasukkan data

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -42,7 +42,32 @@ namespace BinarySearchTree
             }
         }
 
-
+        public void insert(string element) //Insert a node in the binary search tree
+        {
+            Node tmp, parent = null, currentNode = null;
+            search(element, ref parent, ref currentNode);
+            if (currentNode != null) //Check if the node to be inserted already inserted or not
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else //If the specified Node is not parent
+            {
+                tmp = new Node(element, null, null); //creates a Node
+                if (parent == null) //If the trees is empty
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    parent.leftchild = tmp;
+                }
+                else
+                {
+                    parent.rightchild = tmp;
+                }
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
